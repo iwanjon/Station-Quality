dd1fd4b046160f6d293edb9091f551fc4e65e65d
Ferdiahmad3404 <ferdiahmad3404@gmail.com>
2025-08-05 11:20:50 +0700

    Fix conflicts and update dependencies

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -2,13 +2,16 @@ import { Routes, Route } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 import NotFound from '../pages/NotFound';
+import StationQuality from '../pages/StationQuality';
 
 const Router = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/station-quality" element={<StationQuality />} />
       <Route path="/about" element={<About />} />
       <Route path="*" element={<NotFound />} />
+      
     </Routes>
   );
 };
