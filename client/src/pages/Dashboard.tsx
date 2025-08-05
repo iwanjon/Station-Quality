857d7581998eea851076a6f4018bd20bd189e1e2
Ferdiahmad3404 <ferdiahmad3404@gmail.com>
2025-08-05 13:41:41 +0700

    Creating Utilities to API using Axios

--- client/src/pages/Dashboard.tsx
+++ client/src/pages/Dashboard.tsx
@@ -1,10 +1,35 @@
+import { useEffect, useState } from 'react';
 import MainLayout from '../layouts/MainLayout';
+import axiosInstance from '../utilities/Axios';
 
 const Dashboard = () => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    axiosInstance
+      .get('/api/health/')
+      .then((response) => {
+        setData(response.data);
+        setLoading(true);
+      })
+      .catch((err) => {
+        setError('Gagal mengambil data');
+        setLoading(false);
+      });
+  }, []);
+
   return (
     <MainLayout>
-      <h1 className="text-2xl font-bold">Dashboard</h1>
-      <p>Ini konten dashboard.</p>
+      <h1 className="text-2xl font-bold mb-2">Dashboard</h1>
+      {loading && <p>Loading...</p>}
+      {error && <p className="text-red-500">{error}</p>}
+      {data && (
+        <div>
+          <p>Data: {JSON.stringify(data)}</p>
+        </div>
+      )}
     </MainLayout>
   );
 };
