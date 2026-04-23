7ddb4ceaa5b51a5ae835215986045944c98d1c8b
unknown <iwan.affandi.w@gmail.com>
2026-04-23 12:26:23 +0700

    change static url sync with apace

--- privat_dashboard/adminpanel/adminpanel/settings.py
+++ privat_dashboard/adminpanel/adminpanel/settings.py
@@ -149,4 +149,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+# Update this so the admin panel doesn't look like broken text
+STATIC_URL = '/private/admin/static/'
